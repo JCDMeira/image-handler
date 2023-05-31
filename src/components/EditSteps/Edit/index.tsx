8e1ff9afdad562add060a9762fb817480bc4c8f9
Jean Meira <jean.meira10@hotmail.com>
2023-05-30 22:16:45 -0300

    refactor: remove component EditingComands

--- src/components/EditSteps/Edit/index.tsx
+++ src/components/EditSteps/Edit/index.tsx
@@ -1,14 +1,30 @@
-import { EditingComands } from "../../EditingComands";
+import { editImageActions } from "../../../FluxCore/actions/EditImage";
+import { useEditImageStore } from "../../../FluxCore/contexts/imageContext";
+import { AspectRatioBox } from "../../AspectRatioBox";
+import { Button } from "../../Button";
 import { ImageCrop } from "../../ImageCrop";
+import { RotateSlider } from "../../RotateSlider";
 import styles from "./styles.module.scss";
 
 export function Edit() {
+  const { dispatch } = useEditImageStore();
+
+  function handleApply() {
+    dispatch(editImageActions.setStep("download"));
+  }
+
   return (
     <>
       <div className={styles.app_editing_image}>
         <ImageCrop />
       </div>
-      <EditingComands />
+      <div className={styles.comands_container}>
+        <RotateSlider />
+        <AspectRatioBox />
+        <Button onClick={handleApply} variant={"primary_button"}>
+          Apply
+        </Button>
+      </div>
     </>
   );
 }

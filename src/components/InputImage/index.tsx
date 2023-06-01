f15b90684fa426f8ff7f5eb0fd78ffa158c2362a
Jean Meira <jean.meira10@hotmail.com>
2023-05-31 21:47:57 -0300

    feat: add fulldrag

--- src/components/InputImage/index.tsx
+++ src/components/InputImage/index.tsx
@@ -2,6 +2,7 @@ import { editImageActions } from "../../FluxCore/actions/EditImage";
 import { useDropzone } from "react-dropzone";
 import { useEditImageStore } from "../../FluxCore/contexts/imageContext";
 import styles from "./styles.module.scss";
+import { useEffect, useState } from "react";
 
 //@ adicionar drag full
 export function InputImage() {
@@ -14,11 +15,39 @@ export function InputImage() {
       const imageUrl = URL.createObjectURL(imageFile);
       dispatch(editImageActions.setImageName(imageFile.name));
       dispatch(editImageActions.setImageSrc(imageUrl));
+      setIsVisible(false);
     },
   });
 
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("dragover", () => {
+      setIsVisible(true);
+    });
+
+    document.addEventListener("dragleave", () => {
+      setIsVisible(false);
+    });
+
+    return () => {
+      document.removeEventListener("dragover", () => {
+        setIsVisible(true);
+      });
+      document.removeEventListener("dragleave", () => {
+        setIsVisible(false);
+      });
+    };
+  }, []);
+
   return (
     <div className={styles.container_image_input}>
+      {isVisible && (
+        <div className={styles.dragfull} {...getRootProps()}>
+          <h1>Solte seu arquivo em qualquer lugar</h1>
+        </div>
+      )}
+
       <div className={styles.image_input} {...getRootProps()}>
         <input id="image_input" {...getInputProps()} />
       </div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,5 @@ export * from "./Header";
 export * from "./ImageCrop";
 export * from "./InputImage";
 export * from "./RotateSlider";
+
+//@ ajustar o export pelo ts, para sempre importar com @components
